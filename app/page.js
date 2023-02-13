f9af65634fba77741d189d7e747609fc36579016
Rosanna Zhang <rosannazhang92@gmail.com>
2023-02-12 21:55:51 -0800

    Add font style

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <main>
-      <h1 className="text-lg py-2 m-5">Hello</h1>
+      <h1 className="text-lg py-2 m-5">
+        Hello! 
+        <div>
+          My name is Rosanna, a developer based in Vancouver, BC.
+        </div>
+      </h1>
     </main>
   )
 }
