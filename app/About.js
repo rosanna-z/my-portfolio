20dba1ac00be6e1c58ae09a1c8d94029063e0061
Rosanna Zhang <rosannazhang92@gmail.com>
2023-02-17 18:28:15 -0800

    Add fontawesome and add About Me styling

--- app/About.js
+++ app/About.js
@@ -1,28 +1,54 @@
 "use client";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
 export default function About() {
   return (
     <>
-      <h1>ABOUT ME</h1>
-
-      <div className="flex justify-center m-5 w-[500px]">
-        Hi there! My name is Rosanna Zhang. I am a full-stack developer with an
-        interest in front-end development. I have a Bachelor of Management from
-        University of British Columbia and more recently, I completed the
-        Lighthouse Lab's Web Development Bootcamp program. Transitioning from
-        Human Resources, I hope to utilize my people and technical skills to
-        create efficient and effective solutions. I am committed to tackling
-        goals to solve real-world issues to make a positive impact in the
-        community. Here are the technologies I've been working on lately:
-        <ul>
-          <li>Javascript</li>
-          <li>React.js</li>
-          <li>Node.js</li>
-          <li>Next.js</li>
-          <li>Tailwind</li>
-        </ul>
-      </div>
-      <img src="/Rosanna.jpg" alt="profile" height="500" width="250" />
+    {/* <div className="flex flex-col justify-center items-center w-1/2"> */}
+      <h1 className="text-lg mt-20 mx-40 pl-10 font-semibold">ABOUT ME</h1>
+      {/* <section className="flex flex-row"> */}
+      <section className="flex flex-row justify-center mx-10 my-5">
+        <div className="flex flex-col w-1/2">
+        {/* <div className="flex flex-col"> */}
+          Hi there! My name is Rosanna Zhang. I am a full-stack developer with
+          an interest in front-end development. I have a Bachelor of Management
+          from University of British Columbia and more recently, I completed the
+          Lighthouse Lab's Web Development Bootcamp program.
+          <p className="my-5">
+            Transitioning from Human Resources, I hope to utilize my people and
+            technical skills to create efficient and effective solutions. I am
+            committed to tackling goals to solve real-world issues to make a
+            positive impact in the community. Here are some of the technologies I've
+            been working on lately:
+          </p>
+          <ul className="pl-5">
+            <li>
+              <FontAwesomeIcon icon={faCaretRight} className="text-bg-color" />
+              &nbsp;Javascript
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCaretRight} className="text-bg-color" />
+              &nbsp;React.js
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCaretRight} className="text-bg-color" />
+              &nbsp;Node.js
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCaretRight} className="text-bg-color" />
+              &nbsp;Next.js
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCaretRight} className="text-bg-color" />
+              &nbsp;Tailwind
+            </li>
+          </ul>
+        </div>
+        <img src="/Rosanna.jpg" alt="profile" className="px-10 h-64 max-w-md rounded-md" />
+      </section>
+      {/* </div> */}
     </>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,20 +1,26 @@
 "use client";
 
-import NavBar from "./NavBar";
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 import About from "./About";
+import NavBar from "./NavBar";
 
 export default function Home() {
   return (
     <main className="text-grey py-2">
       <NavBar />
-      <div className="bg-bg-color">
+      <section className="bg-bg-color h-80">
       <h1 className="text-lg m-5 py-2">
         Hello! 
         <div>
           My name is <span className="font-bold"> Rosanna Zhang</span>, a developer based in Vancouver, BC.
         </div>
       </h1>
-      </div>
+      </section>
       <About />
     </main>
   )
