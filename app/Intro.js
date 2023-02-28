2d4c73dc58a4a547302cf377d63b53a52af29af5
Rosanna Zhang <rosannazhang92@gmail.com>
2023-02-28 15:32:16 -0800

    Sidebar position: fix and install Typewriter

--- app/Intro.js
+++ app/Intro.js
@@ -1,17 +1,22 @@
 "use client";
 
+import Typewriter from "typewriter-effect";
+
 export default function Intro() {
   return (
     <section className="bg-bg-color">
-      <div className="flex justify-evenly  p-20 mx-36">
-
-      <h1 className="text-lg py-14 px-">
-        Hello!
-        <div>
-          My name is <span className="font-bold"> Rosanna Zhang</span>, a
-          developer based in Vancouver, BC.
+      <div className="flex justify-evenly p-20 py-14 mx-36">
+        <div className="flex flex-col text-5xl py-14 gap-y-5">
+          Hello! This is
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter.typeString(" <span class='font-bold'>Rosanna Zhang,</span>").pauseFor(500).start();
+              }}
+            />
+          <p className="text-3xl">
+            I am a full-stack developer based in Vancouver, BC. 👋
+          </p>
         </div>
-      </h1>
       </div>
     </section>
   );
--- app/Sidebar.js
+++ app/Sidebar.js
@@ -6,16 +6,17 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Sidebar() {
   return (
-    <aside className="flex flex-col flex-start mx-10 gap-5">
+    <aside className="flex flex-col flex-start mx-10 my-64 gap-5 fixed">
       <a href="https://github.com/rosanna-z/">
-        <FontAwesomeIcon icon={faGithub} className="sm:text-sm md:text-xl lg:-text-xl cursor-pointer" />
+        <FontAwesomeIcon icon={faGithub} className="sm:text-sm md:text-xl lg:-text-2xl cursor-pointer" />
       </a>
       <a href="https://www.linkedin.com/in/rosannaz">
-        <FontAwesomeIcon icon={faLinkedin} className="sm:text-sm md:text-xl cursor-pointer" />
+        <FontAwesomeIcon icon={faLinkedin} className="sm:text-sm md:text-xl lg:-text-2xl cursor-pointer" />
       </a>
       <a href="mailto: rosannazhang@hotmail.com">
-        <FontAwesomeIcon icon={faEnvelope} className="sm:text-sm md:text-xl cursor-pointer" />
+        <FontAwesomeIcon icon={faEnvelope} className="sm:text-sm md:text-xl lg:-text-2xl cursor-pointer" />
       </a>
+      {/* <div className="w-0.5 text-black"></div> */}
     </aside>
   );
 }
